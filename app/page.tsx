859f8da600d228c618571a653e5d8a49a1219324
Syamlal CM <syamjayaraj@gmail.com>
2024-02-25 17:05:22 +0530

    mail id changed

--- app/page.tsx
+++ app/page.tsx
@@ -59,8 +59,8 @@ export default function Home() {
       </section>
 
       <section id="three">
-        <a href="mailto:hi@floyet.com" id="sayhi">
-          hi@floyet.com
+        <a href="mailto:info@floyet.com" id="sayhi">
+          info@floyet.com
         </a>
       </section>
       <footer className="footer text-faded text-center py-5">
